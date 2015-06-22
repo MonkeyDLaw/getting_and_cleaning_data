1e1f8cf7af618d8036ace2c9c9f7068e0deb6f82
MonkeyDLaw <james.zhaosi@gmail.com>
2015-06-22 11:18:25 +0800

    first commit

---
+++ run_analysis.R
@@ -0,0 +1,51 @@
+setwd("D:/Rcode/getting and cleaning data/getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset")
+library(plyr)
+
+# Load raw data
+features <- read.table("features.txt", colClasses = c("character"))
+activity_labels <- read.table("activity_labels.txt", col.names = c("ActivityId", "Activity"))
+x_train <- read.table("./train/X_train.txt")
+y_train <- read.table("./train/y_train.txt")
+subject_train <- read.table("./train/subject_train.txt")
+x_test <- read.table("./test/X_test.txt")
+y_test <- read.table("/test/y_test.txt")
+subject_test <- read.table("./test/subject_test.txt")
+
+
+# 1. Merges the training and the test sets to create one data set.
+
+training_sensor_data <- cbind(cbind(x_train, subject_train), y_train)
+test_sensor_data <- cbind(cbind(x_test, subject_test), y_test)
+sensor_data <- rbind(training_sensor_data, test_sensor_data)
+
+sensor_labels <- rbind(rbind(features, c(562, "Subject")), c(563, "ActivityId"))[,2]
+names(sensor_data) <- sensor_labels
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+sensor_data_mean_std <- sensor_data[,grepl("mean|std|Subject|ActivityId", names(sensor_data))]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+
+sensor_data_mean_std <- join(sensor_data_mean_std, activity_labels, by = "ActivityId", match = "first")
+sensor_data_mean_std <- sensor_data_mean_std[,-1]
+
+# 4. Appropriately labels the data set with descriptive names.
+
+names(sensor_data_mean_std) <- gsub('\\(|\\)',"",names(sensor_data_mean_std), perl = TRUE)
+names(sensor_data_mean_std) <- make.names(names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('Acc',"Acceleration",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('GyroJerk',"AngularAcceleration",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('Gyro',"AngularSpeed",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('Mag',"Magnitude",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('^t',"TimeDomain.",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('^f',"FrequencyDomain.",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('\\.mean',".Mean",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('\\.std',".StandardDeviation",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('Freq\\.',"Frequency.",names(sensor_data_mean_std))
+names(sensor_data_mean_std) <- gsub('Freq$',"Frequency",names(sensor_data_mean_std))
+
+# 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+sensor_avg <- ddply(sensor_data_mean_std, c("Subject","Activity"), numcolwise(mean))
+write.table(sensor_avg, file = "sensor_avg.txt")
